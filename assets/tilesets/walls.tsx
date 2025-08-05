8d4aa010dfdbab89d4843767d76f1e6a6965fcf7
Gary <popov.gary@gmail.com>
2025-08-04 20:49:38 -0500

    Restructure Asset Locations

--- assets/tilesets/walls.tsx
+++ assets/tilesets/walls.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="walls" tilewidth="16" tileheight="16" tilecount="117" columns="9">
- <image source="walls.png" width="144" height="208"/>
+ <image source="../tiles/walls.png" width="144" height="208"/>
 </tileset>
