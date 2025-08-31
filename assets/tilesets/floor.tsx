d5e5e16ea5266b5ef8c6fc231c46a399b90c58cc
Gary <popov.gary@gmail.com>
2025-08-31 16:39:19 -0500

    Change collision layer to background layer only

--- assets/tilesets/floor.tsx
+++ assets/tilesets/floor.tsx
@@ -1,4 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="floor" tilewidth="16" tileheight="16" tilecount="4" columns="2">
- <image source="../tiles/floor.png" width="32" height="32"/>
+<tileset version="1.10" tiledversion="1.11.2" name="floor" tilewidth="16" tileheight="16" tilecount="6" columns="3">
+ <image source="../tiles/floor.png" width="48" height="32"/>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
 </tileset>
